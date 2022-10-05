c85289e0e36c52e216d86881f7cf497640aa01b1
gttrcr <gttrcr@gmail.com>
2022-10-05 16:59:46 +0200

    Fix string

--- ExtensionMethodsBuiltinSymbols/ExtensionMethodsBuiltinSymbolsE.cs
+++ ExtensionMethodsBuiltinSymbols/ExtensionMethodsBuiltinSymbolsE.cs
@@ -7,7 +7,7 @@ namespace NetWolf
         ///</summary>
         public static Engine Export(this Engine en, string path, string? name = null)
         {
-            return en.Execute("Export[" + path + ", " + en.ValidNames.Last() + "]", name);
+            return en.Execute("Export[\"" + path + "\", " + en.ValidNames.Last() + "]", name);
         }
     }
 }
