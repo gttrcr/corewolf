2b392dce215f4834288d837952525cdcf3c4d4e4
gttrcr <gttrcr@gmail.com>
2022-10-05 16:56:07 +0200

    Add ExtensionMethodsBuiltinSymbols

---
+++ ExtensionMethodsBuiltinSymbols/ExtensionMethodsBuiltinSymbolsE.cs
@@ -0,0 +1,13 @@
+namespace NetWolf
+{
+    public static class ExtensionMethodsBuiltinSymbolsE
+    {
+        ///<summary>
+        ///export last command
+        ///</summary>
+        public static Engine Export(this Engine en, string path, string? name = null)
+        {
+            return en.Execute("Export[" + path + ", " + en.ValidNames.Last() + "]", name);
+        }
+    }
+}
